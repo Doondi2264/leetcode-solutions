3a631c94b462907d6c0da2011862d27c15e5b95d
V. DOONDI SAMPREETHA <160092159+Doondi2264@users.noreply.github.com>
2026-04-11 17:56:49 +0530

    Time: 8 ms (25.32%), Space: 12.4 MB (61.39%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxNumberOfBalloons(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        f={}
+        for i in text:
+            f[i]=f.get(i,0)+1
+        return min(
+            f.get('b',0),
+            f.get('a',0),
+            f.get('l',0)//2,
+            f.get('o',0)//2,
+            f.get('n',0),
+        )
